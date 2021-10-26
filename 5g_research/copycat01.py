40381c65f081da31dac3a3235adf296560b90525
StephenSo <stephen.spike@gmail.com>
2021-10-26 11:09:43 +0100

    copying local files with python

---
+++ 5g_research/copycat01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import shutil
+import os
+
+def main():
+    # Get current folder
+    WorkDir = os.getcwd()
+
+    # Set current working directory
+    #os.chdir("V:/PythonCourseRepo/mycode/")
+
+    # Copy file sdn_network.txt file to sdn_network.txt.copy
+    shutil.copy("5g_research/sdn_network.txt", "5g_research/sdn_network.txt.copy")
+    shutil.copy("5g_research/sdn_network.txt", "5g_research/sdn_network.txt.copy2")
+
+    # Clone the folder tree to a new location
+    shutil.copytree("5g_research/", "5g_research_backup/",dirs_exist_ok=True)
+
+if __name__ == "__main__":
+    main()

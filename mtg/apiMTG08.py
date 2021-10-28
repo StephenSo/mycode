adfc6168fbd3f470c1f092af25d21dfff954315b
StephenSo <stephen.spike@gmail.com>
2021-10-28 15:44:22 +0100

    Didnt get where I wanted to

--- mtg/apiMTG08.py
+++ mtg/apiMTG08.py
@@ -9,15 +9,19 @@
    https://docs.magicthegathering.io/"""
 
 # imports always go at the top of your code
-import requests
+import requests,os
+from random import randint
 
 # Define our "base" API
 API = "https://api.magicthegathering.io/v1/" # this will never change regardless of the lookup we perform
+# Get current folder
+WorkDir = f"{os.getcwd()}\mtg\"
 
 def main():
     """Run time code"""
 
-    setcode = input("What is the code of the set you are trying to lookup (see mtgsets.index for a list of codes)? ").lower()   # collect user input for MTG card set to lookup
+    #setcode = input("What is the code of the set you are trying to lookup (see mtgsets.index for a list of codes)? ").lower()   # collect user input for MTG card set to lookup
+    setcode = "C16"
 
     # create resp, which is our request object
     resp = requests.get(f"{API}cards?set={setcode}")   # this "f" string reads: API + "cards/" + setcode
@@ -25,8 +29,16 @@ def main():
     # the .json() method will dump a JSON string into Pythonic data structures. COOL!
     # This is much easier than using the urllib.request library
     cards = resp.json()
+    no_of_cards = len(cards['cards'])
+    PAC = randint(0,no_of_cards)
+    card = cards['cards'][PAC]
+    image = requests.get(card['imageUrl'])
+    filename = f"{WorkDir}{card['name']}.png"
 
-    print(cards['cards'])
+    with open(filename, "wb") as cardimage:
+        cardimage.write(image.content)
+    
+    print(f"Your lucky dip is :{card['name']}, card image is in {filename} ")
 
 if __name__ == "__main__":
     main()

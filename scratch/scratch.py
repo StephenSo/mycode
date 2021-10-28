17bf4a1008c80aa4270c979fd7c63f4be9093b2e
StephenSo <stephen.spike@gmail.com>
2021-10-28 16:37:26 +0100

    random choice

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import os,json
-from random import randint
+from random import choice
 
 # Get current folder
 WorkDir = os.getcwd()
@@ -9,9 +9,8 @@ WorkDir = os.getcwd()
 with open(f"{WorkDir}/LOB.json","r") as LOB:
     LOB = json.load(LOB)
 
-no_of_quotes = len(LOB["Quotes"])
-RandomQuote = randint(0,no_of_quotes)
 print("\n\nRandom Life of Brian quote of the day!\n")
-print(LOB["Quotes"][RandomQuote])
+
+print(choice(LOB["Quotes"]))
 print()
 

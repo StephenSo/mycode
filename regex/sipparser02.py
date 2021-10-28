a3c31b4bfa455dcc500f8b74ae66bd6b048f55c5
StephenSo <stephen.spike@gmail.com>
2021-10-28 16:52:48 +0100

    introduction to regex searching with python

---
+++ regex/sipparser02.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+# Python standard library
+import re,requests,os 
+
+WorkDir = f"{os.getcwd()}\\regex\\"
+file_name = "networktrace.txt"
+webdata = "https://static.alta3.com/files/networktrace.txt"
+
+def save_webdata(url,path):
+    resp = requests.get(url)
+    with open(path, "w") as text:
+        text.write(resp.text)
+    return resp
+
+def main():
+    full_path = f"{WorkDir}{file_name}"
+    #Grab the text from a URL
+    save_webdata(webdata,full_path)
+
+    # open the networktrace (text format)
+    with open(full_path) as trace:
+        # loop across the text file
+        for line in trace:
+            # look for a line that begins with sip:+ followed by digits@IP:port
+            conobj = re.search(r'sip:\+(\d+)@\[(.*)\]:?(\d+)?', line)
+            if conobj:
+                print(conobj.group())
+                print("The phone number is...")
+                print(conobj.group(1))
+                print("The IPv6 is...")
+                print(conobj.group(2))
+                print("The port is...")
+                print(conobj.group(3))
+if __name__ == "__main__":
+    main()
